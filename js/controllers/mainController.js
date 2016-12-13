25b8944fe24c9cdd90b894074c4907c1233f860e
stephsen <stephsen59@gmail.com>
2016-12-13 17:52:16 +0100

    todo

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,24 @@
 // MAIN CONTROLLER
-function mainController($scope) {
-    
+function mainController() {
+  this.datas = [];
+    this.add = () => {
+      if ((this.nom === undefined) || (this.prenom === undefined) || (this.email === undefined)) {
+        alert("Veuillez saisir tous les champs");
+      } else {
+        this.datas.push({nom:this.nom, prenom:this.prenom, email:this.email});
+        this.nom ="";
+        this.prenom = "";
+        this.email = "";
+      }
+    };
+
+    this.delete = (index) => {
+      this.datas.splice(index,1);
+    };
+
+    this.update = (index, nom, prenom, email) => {
+      this.datas[index].nom = nom;
+      this.datas[index].prenom = prenom;
+      this.datas[index].email = email;
+    };
 }
